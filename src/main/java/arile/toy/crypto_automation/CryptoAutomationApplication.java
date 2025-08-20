02fcf18f2a69d8f6dafa41f1640c28bc6997a1c2
yesu0208 <dltmddnr0208@gmail.com>
2025-08-20 18:01:17 +0900

    initial commit

--- src/main/java/arile/toy/crypto_automation/CryptoAutomationApplication.java
+++ src/main/java/arile/toy/crypto_automation/CryptoAutomationApplication.java
@@ -2,12 +2,14 @@ package arile.toy.crypto_automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CryptoAutomationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CryptoAutomationApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CryptoAutomationApplication.class, args);
+        SpringApplication.exit(run);
 	}
 
 }
